90f3bc10d7945eab6c8df965ef9cc14610adf374
admase <39431081+admase@users.noreply.github.com>
2020-03-07 23:10:23 -1000

    add tax calc funtion

--- index.php
+++ index.php
@@ -24,11 +24,25 @@
 
   echo "<h2>Items you can afford</h2>";
 
-  foreach($products as $key => $value){
-    if($value <= $credit) {
-      echo "<p>".$key."</p>";
+  foreach ($products as $key => $value) {
+    if ($value <= $credit) {
+      echo "<p>" . $key . "</p>";
     }
   }
+
+  $amount = 800;
+  $taxRate = 0.0825;
+  $addedTax = $amount * $taxRate;
+  echo $addedTax;
+
+  function tax_calc($amount, $tax)
+  {
+    $calculate_tax = $amount * $tax;
+    $amount = round($amount + $calculate_tax, 2);
+    return $amount;
+  }
+
+  echo tax_calc(750, 0.223); 
 
   ?>
 </body>
